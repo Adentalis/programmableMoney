d305adf791686cfd18c545a3749a4ed2dc7a0f48
Daniel <daniel.n96@t-online.de>
2020-05-04 18:14:57 +0200

    fix deposit function

--- bank.sol
+++ bank.sol
@@ -1,11 +1,11 @@
-pragma solidity >=0.5.0 <0.6.0;
-
+pragma solidity 0.6.0;
 
 contract Bank {
     mapping(address => uint256) balances;
 
-    function deposit(uint256 amount) payable public{
-        balances[msg.sender] += amount;
+    function deposit()public payable{
+    	require(msg.value>0);
+        balances[msg.sender]+=msg.value;
     }
 
     function getOwnBalance() public view returns (uint256) {
