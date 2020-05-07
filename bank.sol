a9588d91866c0bcc9100e6d816e39f30f16ec8ab
Daniel <daniel.n96@t-online.de>
2020-05-07 12:13:35 +0200

    implement freeze functionality

--- bank.sol
+++ bank.sol
@@ -2,6 +2,7 @@ pragma solidity 0.6.0;
 
 contract Bank {
     mapping(address => uint256) balances;
+    mapping(address => uint256) freezeEnd;
     mapping(address => string) messages;
     
     //@author Daniel Niemczyk
@@ -12,14 +13,16 @@ contract Bank {
 
     //@author Daniel Niemczyk
     function withdraw() public{
-        require(balances[msg.sender]>0);
+        require(balances[msg.sender] > 0);
+        require(freezeEnd[msg.sender] < now);
         msg.sender.transfer(balances[msg.sender]);
         balances[msg.sender]=0;
     }
 
     //@author Daniel Niemczyk
-    function getOwnBalance() public view returns (uint256) {
-        return balances[msg.sender];
+    function freezeMoney(uint _timeToFreeze) public {
+        //require(balances[msg.sender]>0);
+        freezeEnd[msg.sender]= now + _timeToFreeze;
     }
 
     //@author Leon Bruckert
@@ -28,8 +31,28 @@ contract Bank {
         messages[_address]=_message;
     }
 
+    /*
+    READ-ONLY(view) functions --------------------------------------------------
+    */
+    
+    //@author Daniel Niemczyk
+    function getOwnBalance() public view returns (uint256) {
+        return balances[msg.sender];
+    }
+    
+    //@author Daniel Niemczyk
+    function getOwnFreezeEnd() public view returns (uint256) {
+        return freezeEnd[msg.sender];
+    }
+    
+    //@author Daniel Niemczyk
+    function getCurrentTime() public view returns (uint256) {
+        return now;
+    }
+    
     //@author Leon Bruckert
     function getMessage(address _address) public view returns (string memory) {
         return messages[_address];
     }
+    
 }
