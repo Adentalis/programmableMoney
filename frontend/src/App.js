a70332738945ae3ad3604f2cdfe103e9ec40407a
elianavirgilio <eliana.virgilio@student.fhws.de>
2020-06-09 20:27:45 +0200

    Styling BackgroundColor, grid add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,22 +6,23 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { NavigationBar } from './component/NavigationBar';
 import { Home } from './Home';
-import { About } from './About';
-import { NoMatch } from './NoMatch';
+//import { NoMatch } from './NoMatch';
 import styled from 'styled-components';
 import Tabs from './component/Tabs';
 require('./component/styletab.css');
 
 
 
+
 const GridWrapper = styled.div`
   display: grid;
   
-  grid-gap: 100px;
-  margin-top: 1em;
+ 
+  margin-top: 8em;
   margin-left: 10em;
   margin-right: 6em;
-  grid-template-columns: repeat(25, 2fr);
+  margin-bottom: 10em;
+  grid-template-columns: repeat(25, auto);
   grid-auto-rows: minmax(100px, auto);
 `;
 
@@ -31,6 +32,7 @@ const GridWrapper = styled.div`
 function App() {
   return (
     <React.Fragment>
+      
       <Router>
         <NavigationBar />
 
@@ -38,45 +40,88 @@ function App() {
 
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route component={NoMatch} />
+
         </Switch>
 
       </Router>
 
-      <GridWrapper>
-        <div
-          style={{
-            backgroundColor: '#483d8b',
-            width: '450px',
-            height: '450px',
-            borderRadius: 50,
-
-            padding: '20px',
+      <body className="backgroundColor">
+      <div className="alignment">
 
 
-          }}
-        >
+      
+
+
+          <GridWrapper>
+            <div
+              style={{
+                backgroundColor: '#4B0082',
+                width: '50vh',
+                height: '50vh',
+                borderRadius: 50,
+
+                padding: '20px',
+
+
+              }}
+            >
 
 
 
-          <h1>Geld senden</h1>
-          <Tabs>
-            <div label="Überweisung senden">
-              Zahlungsempfänger , <em>Zahlungsempfänger</em>
+              <h1 style={{
+                color: '#E6E6FA',
+              }}>Geld senden</h1>
+              <Tabs>
+                <div label="Überweisung senden">
+
+                </div>
+                <div label="Termin">
+                  After 'while, <em>Termin</em>!
+      </div>
+                <div label="Dauerauftrag ">
+                  Nothing to see here, this tab is <em>extinct</em>!
+      </div>
+              </Tabs>
+
             </div>
-            <div label="Termin">
-              After 'while, <em>Termin</em>!
+          </GridWrapper>
+
+
+          <GridWrapper>
+            <div
+              style={{
+
+                backgroundColor: '#4B0082',
+                width: '50vh',
+                height: '50vh',
+                borderRadius: 50,
+
+                padding: '20px',
+
+
+              }
+              }
+            >
+              <h1 style={{
+                color: '#E6E6FA',
+              }}>Geld senden</h1>
+              <Tabs>
+                <div label="Überweisung senden">
+
+                </div>
+                <div label="Termin">
+                  After 'while, <em>Termin</em>!
       </div>
-            <div label="Dauerauftrag ">
-              Nothing to see here, this tab is <em>extinct</em>!
+                <div label="Dauerauftrag ">
+                  Nothing to see here, this tab is <em>extinct</em>!
       </div>
-          </Tabs>
+              </Tabs>
+
+            </div>
+          </GridWrapper>
 
         </div>
-      </GridWrapper>
-
-
+      </body>
 
     </React.Fragment>
 
