fdb109858a3899563b61de02fbfd9dbfe7e10577
elianavirgilio <eliana.virgilio@student.fhws.de>
2020-06-09 17:57:02 +0200

    styling and adding from box

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from "react-dom";
-import logo from './logo.svg';
+
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
@@ -8,7 +8,22 @@ import { NavigationBar } from './component/NavigationBar';
 import { Home } from './Home';
 import { About } from './About';
 import { NoMatch } from './NoMatch';
-import { FileTab } from './component/FileTab';
+import styled from 'styled-components';
+import Tabs from './component/Tabs';
+require('./component/styletab.css');
+
+
+
+const GridWrapper = styled.div`
+  display: grid;
+  
+  grid-gap: 100px;
+  margin-top: 1em;
+  margin-left: 10em;
+  margin-right: 6em;
+  grid-template-columns: repeat(25, 2fr);
+  grid-auto-rows: minmax(100px, auto);
+`;
 
 
 
@@ -19,25 +34,52 @@ function App() {
       <Router>
         <NavigationBar />
 
-       
+
 
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
           <Route component={NoMatch} />
         </Switch>
-      </Router>
-      <FileTab>
-        
-      </FileTab>
 
-     
-  
+      </Router>
+
+      <GridWrapper>
+        <div
+          style={{
+            backgroundColor: '#483d8b',
+            width: '450px',
+            height: '450px',
+            borderRadius: 50,
+
+            padding: '20px',
+
+
+          }}
+        >
+
+
+
+          <h1>Geld senden</h1>
+          <Tabs>
+            <div label="Überweisung senden">
+              Zahlungsempfänger , <em>Zahlungsempfänger</em>
+            </div>
+            <div label="Termin">
+              After 'while, <em>Termin</em>!
+      </div>
+            <div label="Dauerauftrag ">
+              Nothing to see here, this tab is <em>extinct</em>!
+      </div>
+          </Tabs>
+
+        </div>
+      </GridWrapper>
+
 
 
     </React.Fragment>
-    
-    
+
   );
 }
 
