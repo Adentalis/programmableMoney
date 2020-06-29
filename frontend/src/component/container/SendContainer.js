f697d2849d684026c7376c3556d0ff063751f3c8
elianavirgilio <eliana.virgilio@student.fhws.de>
2020-06-29 19:14:10 +0200

    send container änderungen

--- frontend/src/component/container/SendContainer.js
+++ frontend/src/component/container/SendContainer.js
@@ -19,45 +19,33 @@ export const SendContainer = () => (
             <div label="Überweisung senden"></div>
             <div label="Termin">
                 <StyledInnerContainer>
-
                     <div class="md-form md-outline input-with-post-icon datepicker">
-
                         <label for="example">Datum auswählen</label>
                         <input placeholder="Select date" type="date" id="example" class="form-control" />
-
                     </div>
-                    
                     <p></p>
                     <p></p>
-
                     <div class="md-form md-outline input-with-post-icon timepicker" darktheme="true">
                         <label for="set-dark-theme">Wähle eine Uhrzeit</label>
-                        <input type="text" id="set-dark-theme" class="form-control" placeholder="00:00"/>
-                            <i class="fas fa-envelope  input-prefix"></i>
+                        <input type="text" id="set-dark-theme" class="form-control" placeholder="00:00" />
+                        <i class="fas fa-envelope  input-prefix"></i>
                     </div>
-
                 </StyledInnerContainer>
                 <button type="button" class="btn btn-secondary btn-lg btn-block">bestätigen</button>
-
             </div>
             <div label="Dauerauftrag ">
                 <StyledInnerContainer>
-
                     <div class="md-form md-outline input-with-post-icon datepicker">
-
                         <label for="example">Datum auswählen</label>
                         <input placeholder="Select date" type="date" id="example" class="form-control" />
-
                     </div>
                     <p></p>
                     <p></p>
-
                     <div class="md-form md-outline input-with-post-icon timepicker" darktheme="true">
                         <label for="set-dark-theme">Wähle eine Uhrzeit</label>
-                        <input type="text" id="set-dark-theme" class="form-control" placeholder="00:00"/>
-                            <i class="fas fa-envelope  input-prefix"></i>
+                        <input type="text" id="set-dark-theme" class="form-control" placeholder="00:00" />
+                        <i class="fas fa-envelope  input-prefix"></i>
                     </div>
-
                 </StyledInnerContainer>
                 <button type="button" class="btn btn-secondary btn-lg btn-block">bestätigen</button>
             </div>
