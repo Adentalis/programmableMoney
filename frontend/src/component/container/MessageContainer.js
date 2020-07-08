f547198a4428f54a34b96928ad8a4e61dc51ea2e
Leon Bruckert <leon.bruckert@gmx.de>
2020-07-08 18:14:39 +0200

    Refactored elianas stuff.

--- frontend/src/component/container/MessageContainer.js
+++ frontend/src/component/container/MessageContainer.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import Tabs from './Tabs';
 import styled from 'styled-components';
-
 import { NAVIGATION_MESSAGE_TEXT } from '../../constants';
 import { Container, Header, Divider, InnerContainer, Content } from './Container';
 require( './textfield.css' );
-require( './styletab.css' );
 
 const StyledContainer = styled(Container)`
     /* Special Style for inner Container */
@@ -21,31 +19,27 @@ export const MessageContainer = () => (
         <Header>{NAVIGATION_MESSAGE_TEXT}</Header>
         <Divider />
         <Content>
-        <Tabs>
-            <div label="Posteingang"  >
-                <StyledInnerContainer>
-                </StyledInnerContainer>
-            </div>
-            <div label="Postausgang">
-                <StyledInnerContainer></StyledInnerContainer>
-            </div>
-            <div label="Neu">
-                <StyledInnerContainer>
-                    <div class="form-group shadow-textarea">
-                         <textarea class="form-control z-depth-1" id="exampleFormControlTextarea6" rows="3" placeholder="Hier Nachricht verfassen"></textarea>
-                    </div>
-
-
-                    <div class="form-group shadow-textarea">
-
-                        <textarea class="form-control z-depth-1" id="exampleFormControlTextarea6" rows="3" placeholder="Hier Absenderadresse hinzufügen"></textarea>
-                    </div>
-                    <button type="button" class="btn btn-secondary center">Abschicken</button>
-                </StyledInnerContainer>
-                <button type="button" class="btn btn-secondary btn-lg btn-block">abschicken</button>
-            </div>
-        </Tabs>
+            <Tabs>
+                <div label="Posteingang"  >
+                    <StyledInnerContainer>
+                    </StyledInnerContainer>
+                </div>
+                <div label="Postausgang">
+                    <StyledInnerContainer></StyledInnerContainer>
+                </div>
+                <div label="Neu">
+                    <StyledInnerContainer>
+                        <div class="form-group shadow-textarea">
+                            <textarea class="form-control z-depth-1" id="exampleFormControlTextarea6" rows="3" placeholder="Hier Nachricht verfassen"></textarea>
+                        </div>
+                        <div class="form-group shadow-textarea">
+                            <textarea class="form-control z-depth-1" id="exampleFormControlTextarea6" rows="3" placeholder="Hier Absenderadresse hinzufügen"></textarea>
+                        </div>
+                        <button type="button" class="btn btn-secondary center">Abschicken</button>
+                    </StyledInnerContainer>
+                    <button type="button" class="btn btn-secondary btn-lg btn-block">abschicken</button>
+                </div>
+            </Tabs>
         </Content>
-
     </StyledContainer>
 )
--- frontend/src/component/container/Tabs.js
+++ frontend/src/component/container/Tabs.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
 import Tab from './Tab';
+require('./styletab.css');
+
 
 class Tabs extends Component {
   static propTypes = {
