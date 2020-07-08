09efc8b5daff63d58dee91512e73bbcf389339de
Leon Bruckert <leon.bruckert@gmx.de>
2020-07-08 20:25:12 +0200

    Tryed get navbar but didnt work

--- frontend/src/component/NavigationBar.js
+++ frontend/src/component/NavigationBar.js
@@ -4,28 +4,30 @@ import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { NAVIGATION_MESSAGE_PATH, NAVIGATION_SEND_PATH, NAVIGATION_FREEZE_PATH, NAVIGATION_MESSAGE_TEXT, NAVIGATION_SEND_TEXT, NAVIGATION_FREEZE_TEXT} from '../constants';
 
 const Style = styled.div`
-  .navbar { 
-    background-color: #4B0082; 
-  }
-  a, 
-  .navbar-nav, 
-  .navbar-light 
-  .nav-link {
-    color: #E6E6FA;
-    &:hover { color: white; }
-  }
-  .navbar-brand {
-    font-size: 1.4em;
-    color: #E6E6FA;
-    &:hover { color: white; }
-  }
-  .form-center {
-    position: absolute !important;
-    left: 25%;
-    right: 25%;
-  }
-`;
+.navbar { 
+  background-color: #4B0082; 
+}
+a, 
+.navbar-nav, 
+.navbar-light 
+.nav-link {
+  color: #E6E6FA;
+  &:hover { color: white; }
+}
+.navbar-brand {
+  font-size: 1.4em;
+  color: #E6E6FA;
+  &:hover { color: white; }
+}
+.form-center {
+  position: absolute !important;
+  left: 25%;
+  right: 25%;
+}
 
+`;
+ 
+  //export const NavigationBar = () => (
 class NavigationBar extends Component {
   render() {
     return (
@@ -43,13 +45,14 @@ class NavigationBar extends Component {
             </Nav>
             <Nav className="ml-auto">
               <NavDropdown.Divider />
-              <Nav.Item>{this.props.account}</Nav.Item>
+              <Nav.Item>
+              Hier Steht deine Adresse
+              </Nav.Item>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
       </Style>
     );
-    }
+  }
 }
-
 export default NavigationBar;
