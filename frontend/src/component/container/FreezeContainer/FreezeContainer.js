a4c8d55f6103e4c4952fff2efcdd494875180d05
Drogo <drogo@Khal.speedport.ip>
2020-07-09 16:34:27 +0100

    refactor Freecontainer

--- frontend/src/component/container/FreezeContainer/FreezeContainer.js
+++ frontend/src/component/container/FreezeContainer/FreezeContainer.js
@@ -1,36 +1,53 @@
-import React  from 'react';
-import styled from 'styled-components';
-import { Container, InnerContainer, Header, Divider, Content, Button } from '../Container';
-import {NAVIGATION_FREEZE_TEXT } from '../../../constants';
+import React, { Component } from "react";
+import { Container, Header, Divider, Content, Button } from "../Container";
+import { NAVIGATION_FREEZE_TEXT } from "../../../constants";
 
-const StyledContainer = styled(Container)`
-    /* Special Style for  Container */
-`;
-const StyledInnerContainer = styled(InnerContainer)`
-    /* Special Style for inner Container */
-`;
-export const StyledButton = styled(Button)`
-    /* Special Style for inner Container */
-    bottom: 50px;
-`;
-export const FreezeContainer = () => (
-    
-    <StyledContainer>
+export default class FreezeContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      freezeTime: "",
+    };
+    this.submitFreezeTime = this.submitFreezeTime.bind(this);
+    this.handleFreezeTimeChange = this.handleFreezeTimeChange.bind(this);
+  }
+
+  handleFreezeTimeChange(e) {
+    console.log("Ein korrektes Datum wurde ausgewählt :)");
+    this.setState({ freezeTime: e.target.value });
+  }
+
+  submitFreezeTime() {
+    var selectedTime = new Date(this.state.freezeTime);
+    var now = new Date();
+    var timeInSecondsToFreeze = Math.floor((selectedTime - now) / 1000);
+    if(timeInSecondsToFreeze > 0)
+    console.log(timeInSecondsToFreeze + " Sekunden zum einfrieren");
+  }
+
+  render() {
+    return (
+      <Container>
         <Header>{NAVIGATION_FREEZE_TEXT}</Header>
         <Divider />
         <Content>
-            <div>
-                <div class="md-form md-outline input-with-post-icon datepicker">  
-                    <label>Datum auswählen</label>
-                    <input placeholder="Select date" type="date" class="form-control"/>
-                </div>
-                <div class="form-group" />
-                        <label for="exampleInputEmail1">Betrag</label>
-                        <input type="betrag" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Betrag eingeben"/>
+          <div>
+            <div class="md-form md-outline input-with-post-icon datepicker">
+              <label>Bis wann soll Ihr Geld eingefroren werden?</label>
+              <input
+                id="freezeTimePicker"
+                placeholder="Select date"
+                type="datetime-local"
+                class="form-control"
+                //value="2020-07-09T13:00"
+                onChange={this.handleFreezeTimeChange}
+              />
             </div>
-            <StyledButton>bestätigen</StyledButton>
+            <div class="form-group" />
+          </div>
+          <Button onClick={this.submitFreezeTime}>Bestätigen</Button>
         </Content>
-    </StyledContainer>
-    
-    
-)
+      </Container>
+    );
+  }
+}
