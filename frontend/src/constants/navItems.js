9157f2750df2e4b0f08bb4fc59be1d3280f2ff89
Drogo <drogo@Khal.speedport.ip>
2020-07-23 10:24:18 +0100

    replace "Gled einfrieren" with "Konto sperren"

--- frontend/src/constants/navItems.js
+++ frontend/src/constants/navItems.js
@@ -5,4 +5,4 @@ export const NAVIGATION_FREEZE_PATH = "freeze";
 export const NAVIGATION_MESSAGE_TEXT = "Nachrichten";
 
 export const NAVIGATION_SEND_TEXT = "Geld senden";
-export const NAVIGATION_FREEZE_TEXT = "Geld einfrieren";
+export const NAVIGATION_FREEZE_TEXT = "Konto sperren";
