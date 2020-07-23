7348b3d65580f4ececdb11e6912c7ec35062cb6f
Drogo <drogo@Khal.speedport.ip>
2020-07-23 23:38:30 +0100

    send correct time to SC

--- frontend/src/component/container/FreezeMoneyContainer/FreezeMoneyContainer.js
+++ frontend/src/component/container/FreezeMoneyContainer/FreezeMoneyContainer.js
@@ -1,20 +1,20 @@
 import React, { Component } from "react";
 import { Container, Header, Divider, Content } from "../Container";
 import { NAVIGATION_FREEZE_MONEY_TEXT } from "../../../constants";
-import {Button, Form, FormControl} from "react-bootstrap";
+import { Button, Form, FormControl } from "react-bootstrap";
 import Tabs from "../Tab/Tabs";
 
 export default class FreezeMoneyContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      freezeValue: '',
+      freezeValue: "",
       freezeValueInvalid: true,
 
-      freezeTime: '',
+      freezeTime: "",
       freezeTimeInvalid: true,
 
-      freezeMessage: '',
+      freezeMessage: "",
       freezeMessageInvalid: true,
 
       transactionValue: "",
@@ -28,7 +28,9 @@ export default class FreezeMoneyContainer extends Component {
     this.submitFreezeTransaction = this.submitFreezeTransaction.bind(this);
 
     this.getNowFormatet = this.getNowFormatet.bind(this);
-    this.sortFreezeTransactionsByReleaseDate = this.sortFreezeTransactionsByReleaseDate.bind(this);
+    this.sortFreezeTransactionsByReleaseDate = this.sortFreezeTransactionsByReleaseDate.bind(
+      this
+    );
     this.MAXIMUM_FREEZE_TRANSACTIONS = 5;
   }
 
@@ -39,7 +41,9 @@ export default class FreezeMoneyContainer extends Component {
     //init the 5 FreezeTransactions
     let getFreezeTransactionKey = [];
     for (let i = 0; i < this.MAXIMUM_FREEZE_TRANSACTIONS; i++) {
-      getFreezeTransactionKey[i] = contract.methods["getFreezeTransaction"].cacheCall(i);
+      getFreezeTransactionKey[i] = contract.methods[
+        "getFreezeTransaction"
+      ].cacheCall(i);
     }
     this.setState({ getFreezeTransactionKey });
   }
@@ -62,59 +66,69 @@ export default class FreezeMoneyContainer extends Component {
 
   handleFreezeTimeChange(e) {
     this.setState({ freezeTime: e.target.value });
-    if((e.target.value).length > 0) {
-      this.setState({freezeTimeInvalid: false});
-    }else{
-      this.setState({freezeTimeInvalid: true});
+    if (e.target.value.length > 0) {
+      this.setState({ freezeTimeInvalid: false });
+    } else {
+      this.setState({ freezeTimeInvalid: true });
     }
   }
 
   handleFreezeMessageChange(e) {
     this.setState({ freezeMessage: e.target.value });
-    if((e.target.value).length > 0) {
-      this.setState({freezeMessageInvalid: false});
-    }else{
-      this.setState({freezeMessageInvalid: true});
+    if (e.target.value.length > 0) {
+      this.setState({ freezeMessageInvalid: false });
+    } else {
+      this.setState({ freezeMessageInvalid: true });
     }
   }
 
-  async sendFreezeTime(value, timeInSecondsToFreeze, message){
-    const { drizzle, drizzleState } = this.props;
-    const contract = drizzle.contracts.Bank;
-    contract.methods["createFreezeTranaction"].cacheSend(timeInSecondsToFreeze, value, message, {
-      from: drizzleState.accounts[0],
+  submitFreezeTransaction() {
+    var selectedTime = new Date(this.state.freezeTime).getTime()/1000;
+    this.createFreezeTranaction(
+      this.state.freezeValue,
+      selectedTime,
+      this.state.freezeMessage
+    );
+
+    this.setState({
+      freezeMessage: "",
+      freezeMessageInvalid: true,
+      freezeValue: "",
+      freezeValueInvalid: true,
+      freezeTime: "",
+      freezeTimeInvalid: true,
     });
   }
 
-  submitFreezeTransaction(){
-    var selectedTime = new Date(this.state.freezeTime);
-    var now = new Date();
-    var timeInSecondsToFreeze = Math.floor((selectedTime - now) / 1000);
-
-    console.log("freeze - Value: " + this.state.freezeValue + " Time " + this.state.freezeTime + "(" + timeInSecondsToFreeze + ") Message" + this.state.freezeMessage);
-    this.sendFreezeTime(this.state.freezeValue, timeInSecondsToFreeze, this.state.freezeMessage);
-
-    this.setState({ freezeMessage: '', freezeMessageInvalid: true, freezeValue: '', freezeValueInvalid: true, freezeTime: '', freezeTimeInvalid: true });
+  async createFreezeTranaction(value, selectedTime, message) {
+    const { drizzle, drizzleState } = this.props;
+    const contract = drizzle.contracts.Bank;
+    contract.methods["createFreezeTranaction"].cacheSend(
+      selectedTime,
+      value,
+      message,
+      {
+        from: drizzleState.accounts[0],
+      }
+    );
   }
 
-  getNowFormatet(){
-    var today = new Date()
-    var tomorrow = new Date(today)
-    tomorrow.setDate(tomorrow.getDate() + 1)
+  getNowFormatet() {
+    var today = new Date();
+    var tomorrow = new Date(today);
+    tomorrow.setDate(tomorrow.getDate() + 1);
     var dd = tomorrow.getDate();
 
-    var mm = tomorrow.getMonth()+1;
+    var mm = tomorrow.getMonth() + 1;
     var yyyy = tomorrow.getFullYear();
-    if(dd<10)
-    {
-      dd='0'+dd;
+    if (dd < 10) {
+      dd = "0" + dd;
     }
 
-    if(mm<10)
-    {
-      mm='0'+mm;
+    if (mm < 10) {
+      mm = "0" + mm;
     }
-    tomorrow = yyyy + '-' + mm + '-' + dd;
+    tomorrow = yyyy + "-" + mm + "-" + dd;
     return tomorrow;
   }
 
@@ -130,7 +144,7 @@ export default class FreezeMoneyContainer extends Component {
     const lastFreezeTransaction = [];
     for (let i = 0; i < this.MAXIMUM_FREEZE_TRANSACTIONS; i++) {
       lastFreezeTransaction[i] =
-          Bank.getFreezeTransaction[this.state.getFreezeTransactionKey[i]];
+        Bank.getFreezeTransaction[this.state.getFreezeTransactionKey[i]];
     }
     //wait for all Transactions loaded
     if (this.allFreezeTransactionsLoaded(lastFreezeTransaction)) {
@@ -138,43 +152,52 @@ export default class FreezeMoneyContainer extends Component {
     }
   }
 
-
   createAllFreezeTransactionContent(allFreezeTransactions) {
     let filledFreezeTransaction = allFreezeTransactions.filter(
-        (freezeTransaction) => freezeTransaction.value[1] !== "0"
+      (freezeTransaction) => freezeTransaction.value[1] !== "0"
     );
-    filledFreezeTransaction = this.sortFreezeTransactionsByReleaseDate(filledFreezeTransaction);
+    filledFreezeTransaction = this.sortFreezeTransactionsByReleaseDate(
+      filledFreezeTransaction
+    );
 
     return filledFreezeTransaction.map((freezeTransaction, index) => (
-        <div key={index}>
-          <hr style={(index != 0) ? {borderTop: "3px solid #bbb"} : {borderTop: "0px solid #bbb"}}/>
+      <div key={index}>
+        <hr
+          style={
+            index != 0
+              ? { borderTop: "3px solid #bbb" }
+              : { borderTop: "0px solid #bbb" }
+          }
+        />
+        <div>
           <div>
-            <div>
-              <b>Zweck</b>
-            </div>
-            <div style={{ paddingLeft: "10px", width: "100%", overflow: "visible"}}>
-              {freezeTransaction.value[2]}
-            </div>
+            <b>Zweck</b>
           </div>
-          <div>
-            <div>
-              <b>Betrag</b>
-            </div>
-            <div style={{ paddingLeft: "10px", width: "100%"}}>
-              {freezeTransaction.value[1]}
-            </div>
-          </div>
-          <div>
-            <div>
-              <b>Freischaltdatum</b>
-            </div>
-            <div style={{ paddingLeft: "10px", width: "100%"}}>
-              {new Date(
-                  1000 * parseInt(freezeTransaction.value[0]) + 3600000
-              ).toLocaleString()}
-            </div>
+          <div
+            style={{ paddingLeft: "10px", width: "100%", overflow: "visible" }}
+          >
+            {freezeTransaction.value[2]}
           </div>
         </div>
+        <div>
+          <div>
+            <b>Betrag</b>
+          </div>
+          <div style={{ paddingLeft: "10px", width: "100%" }}>
+            {freezeTransaction.value[1]}
+          </div>
+        </div>
+        <div>
+          <div>
+            <b>Freischaltdatum</b>
+          </div>
+          <div style={{ paddingLeft: "10px", width: "100%" }}>
+            {new Date(
+              1000 * parseInt(freezeTransaction.value[0]) + 3600000
+            ).toLocaleString()}
+          </div>
+        </div>
+      </div>
     ));
   }
 
@@ -196,12 +219,12 @@ export default class FreezeMoneyContainer extends Component {
             <div label="Neu">
               <label>Welchen Betrag möchten Sie zurücklegen?</label>
               <FormControl
-                  style={{ width: "100%", marginBottom: "10px" }}
-                  type="text"
-                  placeholder="100000000"
-                  className="mr-sm-2"
-                  value={this.state.freezeValue}
-                  onChange={this.handleFreezeValueChange}
+                style={{ width: "100%", marginBottom: "10px" }}
+                type="text"
+                placeholder="100000000"
+                className="mr-sm-2"
+                value={this.state.freezeValue}
+                onChange={this.handleFreezeValueChange}
               />
               <label>Bis wann möchten Sie diesen Betrag zurücklegen?</label>
               <input
@@ -215,28 +238,39 @@ export default class FreezeMoneyContainer extends Component {
               />
               <label>Zweck</label>
               <FormControl
-                  style={{ width: "100%", marginBottom: "10px" }}
-                  type="text"
-                  placeholder="Sparen für schlechte Zeiten"
-                  className="mr-sm-2"
-                  value={this.state.freezeMessage}
-                  onChange={this.handleFreezeMessageChange}
+                style={{ width: "100%", marginBottom: "10px" }}
+                type="text"
+                placeholder="Sparen für schlechte Zeiten"
+                className="mr-sm-2"
+                value={this.state.freezeMessage}
+                onChange={this.handleFreezeMessageChange}
               />
               <Button
-                  style={{
-                    position: "absolute",
-                    width: "calc(100% - 40px)",
-                    marginTop: "10px"
-                  }}
-                  variant="outline-light"
-                  disabled={this.state.freezeValueInvalid || this.state.freezeTimeInvalid || this.state.freezeMessageInvalid}
-                  onClick={this.submitFreezeTransaction}
+                style={{
+                  position: "absolute",
+                  width: "calc(100% - 40px)",
+                  marginTop: "10px",
+                }}
+                variant="outline-light"
+                disabled={
+                  this.state.freezeValueInvalid ||
+                  this.state.freezeTimeInvalid ||
+                  this.state.freezeMessageInvalid
+                }
+                onClick={this.submitFreezeTransaction}
               >
                 Zurücklegen
               </Button>
             </div>
             <div label="Zurückgelegte Beträge">
-              <div style={{width: "calc(100% - 40px)", height: "calc(100% - 100px)", overflow: "auto", position: "absolute" }}>
+              <div
+                style={{
+                  width: "calc(100% - 40px)",
+                  height: "calc(100% - 100px)",
+                  overflow: "auto",
+                  position: "absolute",
+                }}
+              >
                 {this.createFreezeTransactionContent()}
               </div>
             </div>
